7779db52a79f149146751f4601ec34d4a4d7a1e2
hastur <370qvr@gmail.com>
2026-05-12 11:55:04 +0500

    Cheated isr.asm and updated interrupt descriptors table

--- kernel/idt.h
+++ kernel/idt.h
@@ -1,3 +1,9 @@
-// Interrupt Descriptor Table
+#ifndef IDT_H
+#define IDT_H
 
+#include <stdint.h>
 
+void idt_init(void);
+void idt_set_gate(uint8_t vector, uint32_t handler, uint16_t selector, uint8_t flags);
+
+#endif
