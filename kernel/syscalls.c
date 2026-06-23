35223814139206034c7343ae974253fa929a7dc6
hastur <370qvr@gmail.com>
2026-06-23 18:27:13 +0500

    ref(syscalls): Update syscalls.h, add new interface of syscalls. \n - Process type was replaced from proc to process_t from the proc/process.h\n - Updated sysent, add new functions whose are sys_nosys

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -1,11 +1,7 @@
 #include "syscalls.h"
-#include "interrupts.h"
-#include "tty.h"
 
 
-static struct proc kernel_proc = {
-    .pid = 0,
-};
+static int sys_write(process_t* p, void* uap, int* retval);
 
 static int sys_nosys(struct proc* p, void* uap, int* retval) {
     (void)p;
@@ -15,37 +11,45 @@ static int sys_nosys(struct proc* p, void* uap, int* retval) {
     return -1;
 }
 
+static struct sysent sysent[] = {
+    {0, sys_nosys},   // 0
+    {1, sys_nosys},   // SYS_EXIT
+    {0, sys_nosys},   // SYS_FORK
+    {3, sys_nosys},    // SYS_READ
+    {3, sys_write},   // SYS_WRITE
+    // {2, sys_open},    // SYS_OPEN
+    // {1, sys_close},   // SYS_CLOSE
+};
+
+static process_t kernel_proc;
+
 static inline long syscall1(long num, long a1) {
     long ret;
     __asm__ volatile ("int $0x80" : "=a"(ret) : "a"(num), "b"(a1) : "memory");
     return ret;
 }
-
 static inline long syscall2(long num, long a1, long a2) {
     long ret;
     __asm__ volatile ("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2) : "memory");
     return ret;
 }
-
 static inline long syscall3(long num, long a1, long a2, long a3) {
     long ret;
     __asm__ volatile ("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2), "d"(a3) : "memory");
     return ret;
 }
-
 static inline long syscall4(long num, long a1, long a2, long a3, long a4) {
     long ret;
     __asm__ volatile ("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2), "d"(a3), "S"(a4) : "memory");
     return ret;
 }
-
 static inline long syscall5(long num, long a1, long a2, long a3, long a4, long a5) {
     long ret;
     __asm__ volatile ("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2), "d"(a3), "S"(a4), "D"(a5) : "memory");
     return ret;
 }
 
-static int sys_write(struct proc* p, void* uap, int* retval) {
+static int sys_write(process_t* p, void* uap, int* retval) {
     struct sys_write_args* args = (struct sys_write_args*)uap;
 
     (void)p;
@@ -67,14 +71,6 @@ static int sys_write(struct proc* p, void* uap, int* retval) {
     *retval = (int)args->len;
     return 0;
 }
-
-static struct sysent sysent[] = {
-    {0, sys_nosys},  // 0
-    {1, sys_nosys},  // 1 = exit
-    {0, sys_nosys},  // 2 = fork
-    {3, sys_nosys},  // 3 = read
-    {3, sys_write},  // 4 = write
-};
 
 void syscall_dispatch(struct interrupt_frame* frame) {
     uint32_t number = frame->eax;
@@ -103,7 +99,6 @@ void syscall_dispatch(struct interrupt_frame* frame) {
 void syscalls_init(void) {
     interrupt_register_handler(SYSCALL_VECTOR, syscall_dispatch);
 }
-
 
 int write(int fd, const char* buf, uint32_t len) {
     return (int)syscall3(SYS_WRITE, fd, (long)buf, len);
--- kernel/syscalls.h
+++ kernel/syscalls.h
@@ -3,33 +3,54 @@
 
 #include <stdint.h>
 #include "interrupts.h"
+#include "tty.h"
+#include "proc/process.h"
+#include "fs/vfs.h"
 
 #define SYSCALL_VECTOR 0x80
 
-#define SYS_EXIT 1
-#define SYS_READ 3
-#define SYS_WRITE 4
-
-
-struct proc {
-    int pid;
-};
+#define SYS_EXIT   1
+#define SYS_FORK   2
+#define SYS_READ   3
+#define SYS_WRITE  4
+#define SYS_OPEN   5
+#define SYS_CLOSE  6
 
 typedef int syscallarg_t;
-typedef int (*sys_call_t)(struct proc* p, void* uap, int* retval);
+typedef int (*sys_call_t)(process_t *p, void *uap, int *retval);
 
 struct sysent {
     int sys_narg;
     sys_call_t sys_call;
 };
 
-struct sys_write_args {
+struct sys_read_args {
     int fd;
-    const char* buf;
+    void *buf;
     uint32_t len;
 };
 
+struct sys_write_args {
+    int fd;
+    const char *buf;
+    uint32_t len;
+};
+
+struct sys_open_args {
+    const char *path;
+    int flags;
+};
+
+struct sys_close_args {
+    int fd;
+};
+
 void syscalls_init(void);
-void syscall_dispatch(struct interrupt_frame* frame);
-int write(int fd, const char* buf, uint32_t len);
+void syscall_dispatch(struct interrupt_frame *frame);
+
+int open(const char *path, int flags);
+int close(int fd);
+int read(int fd, void *buf, uint32_t len);
+int write(int fd, const char *buf, uint32_t len);
+
 #endif
